ff6725db062f66551f1472a53908d3b9854e474b
braincee <kwesi938@gmail.com>
2022-08-02 03:24:22 +0100

    add changes to music and genre classes

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,17 +1,16 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_reader :album_name, :artist_name, :publish_name, :on_spotify
+  attr_reader :album_name, :artist_name, :publish_name, :on_spotify
 
-    def initialize(album_name, artist_name, publish_name, on_spotify)
-        super(publish_date)
-        @album_name = album_name
-        @artist_name = artist_name
-        @on_spotify = on_spotify
-    end
+  def initialize(album_name, artist_name, _publish_name, on_spotify)
+    super(publish_date)
+    @album_name = album_name
+    @artist_name = artist_name
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
-
