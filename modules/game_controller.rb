d77af1042ea75da6d1da1d93802f21c2dd2c1866
KanoCode <kanokanombola@gmail.com>
2022-08-02 17:47:21 +0200

    fix linter errors

--- modules/game_controller.rb
+++ modules/game_controller.rb
@@ -5,16 +5,13 @@ module GameController
     print 'Enter the publish date of the game(year-month-day): '
     publish_date = gets.chomp.to_s
     print 'Enter last played date of the game: '
-    last_played_at = gets.chomp.to_s
-    print 'The game is multiplayer ? (y/n): '
+    last_played_at = gets.chomp.to_s print 'The game is multiplayer ? (y/n): '
     multiplayer = nil
     while multiplayer.nil?
       multiplayer = gets.chomp.to_s
       case multiplayer
-      when 'y'
-        multiplayer = true
-      when 'n'
-        multiplayer = false
+      when 'y' then multiplayer = true
+      when 'n' then multiplayer = false
       else
         print 'Please enter y or n: '
         multiplayer = nil
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,12 +1,11 @@
 require './classes/author'
 
 describe Author do
-    before :each do
-        @author = Author.new("john","Doe")
-      end
+  before :each do
+    @author = Author.new('john', 'Doe')
+  end
 
-      it 'items array should contain instance of author' do
-        expect(@author).to be_an_instance_of Author
-      end
-
+  it 'items array should contain instance of author' do
+    expect(@author).to be_an_instance_of Author
+  end
 end
