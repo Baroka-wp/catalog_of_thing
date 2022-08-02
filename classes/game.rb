4b58c13a441271ecf8dc8cb0ff8b9761444022f7
KanoCode <kanokanombola@gmail.com>
2022-08-02 17:17:40 +0200

    Add private keyword to private method

--- classes/game.rb
+++ classes/game.rb
@@ -1,43 +1,18 @@
-require_relative 'item'
+require './item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at, :name, :publish_date
+  attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date:, last_played_at:, name: 'Unknown', archived: false, multiplayer: false)
-    super(publish_date: publish_date, archived: archived, name: name)
+  def initialize(name, multiplayer, last_played_at, publish_date)
+    super(name, publish_date)
+    @id = id || Random.rand(1..100)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
 
+  private
+
   def can_be_archived?
-    super && (Time.now.year - @last_played_at.year) > 2
+    super && Date.today.year - @last_played_at.year > 2
   end
-
-  private :can_be_archived?
-
-  def to_s
-    "[GAME] Multiplayer: #{multiplayer}, Last played at: #{@last_played_at}"
-  end
-
-  def to_json(*args)
-    {
-      JSON.create_id => self.class.name,
-      'name' => @name,
-      'id' => @id,
-      'multiplayer' => @multiplayer,
-      'publish_date' => @publish_date,
-      'last_played_at' => @last_played_at
-    }.to_json(*args)
-  end
-end
-
-def to_json(*args)
-  {
-    JSON.create_id => self.class.name,
-    'name' => @name,
-    'id' => @id,
-    'multiplayer' => @multiplayer,
-    'publish_date' => @publish_date,
-    'last_played_at' => @last_played_at
-  }.to_json(*args)
 end
