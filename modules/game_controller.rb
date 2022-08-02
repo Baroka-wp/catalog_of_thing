cd209f7cedfb53dd8692504912352e5c5c37493e
KanoCode <kanokanombola@gmail.com>
2022-08-02 17:15:57 +0200

    modify create_game function

--- modules/game_controller.rb
+++ modules/game_controller.rb
@@ -1,6 +1,27 @@
 module GameController
-  def create_game; end
-
-  def load_games; end
-
+  def create_game
+    print 'Enter the name of the game: '
+    name = gets.chomp.to_s
+    print 'Enter the publish date of the game(year-month-day): '
+    publish_date = gets.chomp.to_s
+    print 'Enter last played date of the game: '
+    last_played_at = gets.chomp.to_s
+    print 'The game is multiplayer ? (y/n): '
+    multiplayer = nil
+    while multiplayer.nil?
+      multiplayer = gets.chomp.to_s
+      case multiplayer
+      when 'y'
+        multiplayer = true
+      when 'n'
+        multiplayer = false
+      else
+        print 'Please enter y or n: '
+        multiplayer = nil
+      end
+    end
+    game = Game.new(name, multiplayer, last_played_at, publish_date)
+    @items << game
+    puts '✅ Game added successfully !'
+  end
 end
