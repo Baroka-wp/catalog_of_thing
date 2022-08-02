c04219a1bfeb1b6e873cc02cf4d373c2a2092955
braincee <kwesi938@gmail.com>
2022-08-02 02:31:57 +0100

    Add Music and Genre Tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE ruby_catalog;
+
 CREATE TABLE items (
     id SERIAL PRIMARY KEY,
     published_date DATE,
@@ -6,6 +7,7 @@ CREATE TABLE items (
     genre_id INTEGER,
     author_id INTEGER,
     CONSTRAINT fk_label FOREIGN KEY(label_id) REFERENCES label(id),
+    CONSTRAINT genre_fk FOREIGN KEY(genre_id) REFERENCES genres(id)
 );
 CREATE TABLE label(
     id SERIAL PRIMARY KEY,
@@ -23,3 +25,20 @@ CREATE TABLE book(
     genre_id INT,
     CONSTRAINT fk_label FOREIGN KEY(label_id) REFERENCES label(id),
 );
+
+CREATE TABLE genres (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    items TEXT []
+);
+
+CREATE TABLE music_albums (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    published_date DATE,
+    archived BOOLEAN,
+    on_spotify BOOLEAN,
+    genre_id INT,
+    CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
